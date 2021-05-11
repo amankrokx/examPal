66dda5ec904a9f81dbd6a9a4058c8ef4cfbe439e
Aman Kumar <amankumar.spj410@gmail.com>
2021-05-11 10:26:27 +0530

    aman

---
+++ public/index.js
@@ -0,0 +1,51 @@
+window.onload = () => {
+    tl = document.getElementById('loader')
+    ht()
+    let items = document.getElementsByTagName('form')
+    toggle.onclick = () => {
+        items[0].classList.toggle('hidden')
+        items[1].classList.toggle('hidden')
+    }
+    document.querySelectorAll('form span.toggle').forEach(e => {
+        e.onclick = () => {
+            items[1].classList.toggle('hidden')
+            items[3].classList.toggle('hidden')
+        }
+    });
+
+    /*
+    const loadEl = document.querySelector('#load');
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    // // The Firebase SDK is initialized and available here!
+    //
+    // firebase.auth().onAuthStateChanged(user => { });
+    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+    // firebase.firestore().doc('/foo/bar').get().then(() => { });
+    // firebase.functions().httpsCallable('yourFunction')().then(() => { });
+    // firebase.messaging().requestPermission().then(() => { });
+    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+    // firebase.analytics(); // call to activate
+    // firebase.analytics().logEvent('tutorial_completed');
+    // firebase.performance(); // call to activate
+    //
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+    try {
+      let app = firebase.app();
+      let features = [
+        'auth', 
+        'database', 
+        'firestore',
+        'functions',
+        'messaging', 
+        'storage', 
+        'analytics', 
+        'remoteConfig',
+        'performance',
+      ].filter(feature => typeof app[feature] === 'function');
+      loadEl.textContent = `Firebase SDK loaded with ${features.join(', ')}`;
+    } catch (e) {
+      console.error(e);
+      loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
+    }*/
+  }
